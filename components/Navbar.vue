2e5e85d668c5539238ee6ab7df0ab5fd4b73ec55
Java Software Engineer <38032516+acarmenis@users.noreply.github.com>
2026-08-24 11:56:59 +0300

    Add files via upload

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <a class="logo" href="#">AK<span>.</span></a>
+    <div class="links">
+      <a href="#experience">Experience</a>
+      <a href="#work">Work</a>
+      <a href="#about">About</a>
+      <a href="#stack">Stack</a>
+      <a href="#contact">Contact</a>
+    </div>
+  </nav>
+</template>
