27a55c40d6c46ae91bf18bcc14ab9076fee70153
Java Software Engineer <38032516+acarmenis@users.noreply.github.com>
2026-09-01 12:20:14 +0300

    Update ProjectCard.vue

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,13 +9,33 @@ defineProps<{
 
 <template>
   <article class="card project">
+
     <div>
-      <span v-if="category" class="project-category">{{ category }}</span>
+
+      <span
+        v-if="category"
+        class="project-category"
+      >
+        {{ category }}
+      </span>
+
       <h3>{{ title }}</h3>
+
       <p>{{ description }}</p>
+
       <div class="tags">
-        <span v-for="tag in tags" :key="tag" class="tag">{{ tag }}</span>
+
+        <span
+          v-for="tag in tags"
+          :key="tag"
+          class="tag"
+        >
+          {{ tag }}
+        </span>
+
       </div>
+
     </div>
+
   </article>
 </template>
